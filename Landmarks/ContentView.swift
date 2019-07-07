a0167f6c23ee9bab0b74ecf0c2a7d3791f578164
新井進鎬 <chino19810618@gmail.com>
2019-07-07 13:12:40 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by chino on 2019/07/07.
+//  Copyright © 2019 chino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
